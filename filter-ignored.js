235335d559a22d175fa808bb214c9bcb9deb859a
Dexter Shepherd <dexterjshepherd@gmail.com>
2021-02-27 19:04:12 -0800

    Add scripts

---
+++ filter-ignored.js
@@ -0,0 +1,22 @@
+// filter-ignored.js
+var split = require('split')
+var CLIEngine = require('eslint').CLIEngine
+var cli = new CLIEngine({})
+const process = require('process')
+const execSync = require('child_process').execSync
+
+// process.stdin.setEncoding('utf8');
+// process.stdin.pipe(split()).on('data', function (file) {
+//     if (!cli.isPathIgnored(file)) console.log(file);
+// });
+//
+//
+
+const parsed = process.argv
+  .slice(2)
+  .filter(file => !cli.isPathIgnored(file))
+  .join(' ')
+
+if (parsed.length) {
+  execSync(`./node_modules/eslint/bin/eslint.js ${parsed}`, { stdio: 'inherit', cwd: '.' })
+}
